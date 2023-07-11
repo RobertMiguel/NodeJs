d7589889d2d2eb761b232fb29a46d41ff5a455bb
RobertMiguel <robertresende9@gmail.com>
2023-07-11 09:20:31 -0300

    module path

--- core_modules/http.ts
+++ core_modules/http.ts
@@ -1,14 +1,11 @@
 import * as http from 'http'
-import { URL } from 'url'
+import * as url from 'url'
 
 const port = 8080
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  const reqUrl = new URL(`http://${req.headers.host}${req.url}`);
-  const queryParams = reqUrl.searchParams
-  const name = queryParams.get('name')
-  res.end(`Hello, ${name || 'World'} !`)
+  const q = url.parse(req.url, true).query
 })
 
 server.listen(port, () => {

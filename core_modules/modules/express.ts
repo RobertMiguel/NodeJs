d3c175062449ac2e025a50726d26874041aad9ed
RobertMiguel <robertresende9@gmail.com>
2023-07-12 21:35:56 -0300

    failed authentication mongodb

--- core_modules/modules/express.ts
+++ core_modules/modules/express.ts
@@ -7,3 +7,21 @@ export function getHomePage(req: Required, res: Response) {
 export function getAboutPage(req: Required, res: Response) {
   res.status(200).send('<h1>About Page</h1>')
 }
+
+export function getUsers(req: Required, res: Response) {
+  const users = [
+    {
+      name: 'Robert',
+      age: 17,
+      email: 'robert@gmail.com'
+    },
+    {
+      name: 'Miguel',
+      age: 17,
+      email: 'miguel@gmail.com'
+    }
+  ]
+
+  res.status(200).json(users)
+
+}

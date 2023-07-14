1923cfafb2e95ed7dc8e4ce308bbb52f45ab57e4
RobertMiguel <robertresende9@gmail.com>
2023-07-14 13:09:12 -0300

    Tradutions Yup

--- books_directory/src/server/controllers/books/Create.ts
+++ books_directory/src/server/controllers/books/Create.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+
+
 
 interface IBooks {
   name: string;
@@ -6,10 +10,44 @@ interface IBooks {
   register: string
 }
 
-export const create = (req: Request<{}, {}, IBooks>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<IBooks> = yup.object().shape({
+  name: yup.string().required().min(3),
+  value: yup.number().required().min(3),
+  register: yup.string().required().min(3),
 
+});
+
+export const create = async (req: Request<{}, {}, IBooks>, res: Response) => {
+
+
+  const validateData: IBooks | undefined = undefined;
 
   console.log(req.body.name);
 
+  try {
+    await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch (err) {
+
+    const yupError = err as yup.ValidationError;
+
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach(error => {
+
+      if (!error.path) return;
+
+
+
+      errors[error.path] = error.message;
+
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+
+  }
+
+  console.log(validateData);
+
   return res.send('Create !');
+
 };
